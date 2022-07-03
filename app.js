1be27378258451fdfe4f4e6cf91489a19f599aca
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-07-03 21:59:04 +0800

    feat: add login route & edit.hbs& new.hbs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -11,8 +14,17 @@ const app = express()
 const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -35,9 +47,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
